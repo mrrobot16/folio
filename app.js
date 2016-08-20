0ec27c9b3f013378f290a373ebc0d6f0882ffa63
= <hmgaar@gmail.com>
2016-08-20 08:15:57 -0400

    rebuilt version of portfolio

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 5000
+var app = express();
+
+app.use(express.static(__dirname + "/node_modules"));
+app.use(express.static('assets'));
+
+app.get('/', function(req, res) {
+        res.sendfile('index.html');
+    });
+app.listen(port);
+module.exports = app;
